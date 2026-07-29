a70e64cdd5ba0a41cb879599f4435bd96c720046
arthuur-gomes <arthuroliveiragomes430@gmail.com>
2026-07-29 12:55:29 -0300

    refactor: Utiliza @Data para GET/SET, e ajusta estratégia do ID para IDENTITY

--- src/main/java/dev/java10x/MagicFridgeAI/Model/FoodItem.java
+++ src/main/java/dev/java10x/MagicFridgeAI/Model/FoodItem.java
@@ -2,6 +2,7 @@ package dev.java10x.MagicFridgeAI.Model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
@@ -10,54 +11,17 @@ import java.time.LocalDate;
 @Table(name = "tb_food_item")
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class FoodItem {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String categoria;
     private Integer quantidade;
+    @Column(name="DATA_VALIDADE")
     private LocalDate dataValidade;
 
-    public Long getId() {
 
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCategoria() {
-        return categoria;
-    }
-
-    public void setCategoria(String categoria) {
-        this.categoria = categoria;
-    }
-
-    public Integer getQuantidade() {
-        return quantidade;
-    }
-
-    public void setQuantidade(Integer quantidade) {
-        this.quantidade = quantidade;
-    }
-
-    public LocalDate getDataValidade() {
-        return dataValidade;
-    }
-
-    public void setDataValidade(LocalDate dataValidade) {
-        this.dataValidade = dataValidade;
-    }
 }
