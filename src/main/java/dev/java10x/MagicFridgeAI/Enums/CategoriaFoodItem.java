0b12c33c762d3c2e1744351058c2efcf0a21aed2
arthuur-gomes <arthuroliveiragomes430@gmail.com>
2026-08-12 13:00:45 -0300

    refactor: Cria Enum de categoria

---
+++ src/main/java/dev/java10x/MagicFridgeAI/Enums/CategoriaFoodItem.java
@@ -0,0 +1,12 @@
+package dev.java10x.MagicFridgeAI.Enums;
+
+public enum CategoriaFoodItem {
+
+    LATICICIOS,
+    CARNES_OVOS,
+    GRAOS_CEREAIS,
+    LEGUMINOSAS,
+    HORTALICAS,
+    FRUTAS,
+    OLEAGINOSAS
+}
